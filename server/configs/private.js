0903793c275704c392ebb326c299b57d19f5262f
liushang233 <664913676@qq.com>
2017-08-28 09:28:16 +0800

    添加私有配置文件

---
+++ server/configs/private.js
@@ -0,0 +1,23 @@
+// 这个是私有配置
+// module.exports = {
+//     app: {
+//         port: process.env.PORT || 3001,
+//         routerPrefixApi: '/api'
+//     },
+//     mongodb: {
+//         url: 'mongodb://127.0.0.1:27017/v-k-blog',
+//         secret: {           // 非必须
+//             user: '',
+//             pwd: ''
+//         }
+//     },
+//     jwt: {
+//         secret: 'secrets',
+//         expiresIn: '1h'
+//     },
+//     admin: {
+//         username: 'admin',
+//         password: 'admin123',
+//         name: 'liushang'
+//     }
+// };
