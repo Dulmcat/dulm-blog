74f792c028566e33f32195f229042ceae61a8daa
liushang233 <664913676@qq.com>
2017-08-28 14:49:17 +0800

    前端-admin

---
+++ admin/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+    <div id="app">
+        <transition name="fade">
+            <router-view></router-view>
+        </transition>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'app'
+}
+</script>
+
+<style lang="less">
+.fade-enter-active, .fade-leave-cative{
+    transition: opacity .5s;
+}
+.fade-enter, .fade-leave-active{
+    opacity: 0;
+}
+</style>
