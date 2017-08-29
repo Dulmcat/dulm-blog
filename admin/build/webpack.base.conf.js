069cc620c81ecc398ebd8ff42a412b37577c2d9e
liushang233 <664913676@qq.com>
2017-08-29 13:51:31 +0800

    完成打包相关配置

--- admin/build/webpack.base.conf.js
+++ admin/build/webpack.base.conf.js
@@ -35,7 +35,13 @@ module.exports = {
       {
         test: /\.js$/,
         loader: 'babel-loader',
-        include: [resolve('src'), resolve('test')]
+        include: [
+          resolve('src'),
+          resolve('test'),
+          resolve('libs'),
+          resolve('/node_modules/element-ui/packages'),
+          resolve('/node_modules/element-ui/src')
+        ]
       },
       {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
