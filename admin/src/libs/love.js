a7ee80cbed7f243a53c93644a0cec77d7565140c
Liushang <dulmcat@gmail.com>
2019-02-15 16:55:58 +0800

    love.js

--- admin/src/libs/love.js
+++ admin/src/libs/love.js
@@ -1,69 +1,115 @@
 // <!-- 为页面添加爱心特效 -->
-export default function love(){
-    var hearts = [];
-    window.requestAnimationFrame = (function () {
-        return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame ||
-            function (callback) {
-                setTimeout(callback, 1000 / 60);
-            }
-    })();
-    init();
+export default function love() {
+  var hearts = [];
+  window.requestAnimationFrame = (function () {
+    return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame ||
+      function (callback) {
+        setTimeout(callback, 1000 / 60);
+      }
+  })();
+  init();
 
-    function init() {
-        css(".heart{width: 10px;height: 10px;position: fixed;background: #f00;transform: rotate(45deg);-webkit-transform: rotate(45deg);-moz-transform: rotate(45deg);}.heart:after,.heart:before{content: '';width: inherit;height: inherit;background: inherit;border-radius: 50%;-webkit-border-radius: 50%;-moz-border-radius: 50%;position: absolute;}.heart:after{top: -5px;}.heart:before{left: -5px;}");
-        attachEvent();
-        gameloop();
-    }
+  function init() {
+    css(".heart{width: 10px;height: 10px;position: fixed;background: #f00;transform: rotate(45deg);-webkit-transform: rotate(45deg);-moz-transform: rotate(45deg);}.heart:after,.heart:before{content: '';width: inherit;height: inherit;background: inherit;border-radius: 50%;-webkit-border-radius: 50%;-moz-border-radius: 50%;position: absolute;}.heart:after{top: -5px;}.heart:before{left: -5px;}");
+    attachEvent();
+    gameloop();
+  }
 
-    function gameloop() {
-        for (var i = 0; i < hearts.length; i++) {
-            if (hearts[i].alpha <= 0) {
-                document.body.removeChild(hearts[i].el);
-                hearts.splice(i, 1);
-                continue;
-            }
-            hearts[i].y--;
-            hearts[i].scale += 0.004;
-            hearts[i].alpha -= 0.013;
-            hearts[i].el.style.cssText = "left:" + hearts[i].x + "px;top:" + hearts[i].y + "px;opacity:" + hearts[i].alpha + ";transform:scale(" + hearts[i].scale + "," + hearts[i].scale + ") rotate(45deg);background:" + hearts[i].color;
-        }
-        requestAnimationFrame(gameloop);
+  function gameloop() {
+    for (var i = 0; i < hearts.length; i++) {
+      if (hearts[i].alpha <= 0) {
+        document.body.removeChild(hearts[i].el);
+        hearts.splice(i, 1);
+        continue;
+      }
+      hearts[i].y--;
+      hearts[i].scale += 0.004;
+      hearts[i].alpha -= 0.013;
+      hearts[i].el.style.cssText = "left:" + hearts[i].x + "px;top:" + hearts[i].y + "px;opacity:" + hearts[i].alpha + ";transform:scale(" + hearts[i].scale + "," + hearts[i].scale + ") rotate(45deg);background:" + hearts[i].color;
     }
+    requestAnimationFrame(gameloop);
+  }
 
-    function attachEvent() {
-        var old = typeof window.onclick === "function" && window.onclick;
-        window.onclick = function (event) {
-            old && old();
-            createHeart(event);
-        }
+  function attachEvent() {
+    var old = typeof window.onclick === "function" && window.onclick;
+    window.onclick = function (event) {
+      old && old();
+      createHeart(event);
     }
+  }
 
-    function createHeart(event) {
-        var d = document.createElement("div");
-        d.className = "heart";
-        hearts.push({
-            el: d,
-			x: event.clientX - 5,
-			y: event.clientY - 5,
-			scale: 1,
-			alpha: 1,
-			color: randomColor()
-        });
-        document.body.appendChild(d);
-    }
+  function createHeart(event) {
+    var d = document.createElement("div");
+    d.className = "heart";
+    hearts.push({
+      el: d,
+      x: event.clientX - 5,
+      y: event.clientY - 5,
+      scale: 1,
+      alpha: 1,
+      color: randomColor()
+    });
+    document.body.appendChild(d);
+  }
 
-    function css(css) {
-        var style = document.createElement("style");
-        style.type = "text/css";
-        try {
-            style.appendChild(document.createTextNode(css));
-        } catch (ex) {
-            style.styleSheet.cssText = css;
-        }
-        document.getElementsByTagName('head')[0].appendChild(style);
+  function css(css) {
+    var style = document.createElement("style");
+    style.type = "text/css";
+    try {
+      style.appendChild(document.createTextNode(css));
+    } catch (ex) {
+      style.styleSheet.cssText = css;
     }
+    document.getElementsByTagName('head')[0].appendChild(style);
+  }
 
-    function randomColor() {
-        return "rgb(" + (~~(Math.random() * 255)) + "," + (~~(Math.random() * 255)) + "," + (~~(Math.random() * 255)) + ")";
-    }
+  function randomColor() {
+    return "rgb(" + (~~(Math.random() * 255)) + "," + (~~(Math.random() * 255)) + "," + (~~(Math.random() * 255)) + ")";
+  }
+
+  document.addEventListener('touchmove', function (e) {
+    e.preventDefault()
+  })
+  var c = document.getElementsByTagName('canvas')[0],
+    x = c.getContext('2d'),
+    pr = window.devicePixelRatio || 1,
+    w = window.innerWidth,
+    h = window.innerHeight,
+    f = 90,
+    q,
+    m = Math,
+    r = 0,
+    u = m.PI * 2,
+    v = m.cos,
+    z = m.random
+  c.width = w * pr
+  c.height = h * pr
+  x.scale(pr, pr)
+  x.globalAlpha = 0.6
+  function i() {
+    x.clearRect(0, 0, w, h)
+    q = [{ x: 0, y: h * .7 + f }, { x: 0, y: h * .7 - f }]
+    while (q[1].x < w + f) d(q[0], q[1])
+  }
+  function d(i, j) {
+    x.beginPath()
+    x.moveTo(i.x, i.y)
+    x.lineTo(j.x, j.y)
+    var k = j.x + (z() * 2 - 0.25) * f,
+      n = y(j.y)
+    x.lineTo(k, n)
+    x.closePath()
+    r -= u / -50
+    x.fillStyle = '#' + (v(r) * 127 + 128 << 16 | v(r + u / 3) * 127 + 128 << 8 | v(r + u / 3 * 2) * 127 + 128).toString(16)
+    x.fill()
+    q[0] = q[1]
+    q[1] = { x: k, y: n }
+  }
+  function y(p) {
+    var t = p + (z() * 2 - 1.1) * f
+    return (t > h || t < 0) ? y(p) : t
+  }
+  document.onclick = i
+  document.ontouchstart = i
+  i()
 }
